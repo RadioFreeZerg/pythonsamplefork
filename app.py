8371551e1936bcc8b4fcd5f7433b189a8ac2369d
HP <hectorperezfuentes@gmail.com>
2025-05-05 01:46:36 -0400

    Adding hidden place to app py

--- app.py
+++ app.py
@@ -7,6 +7,10 @@ app = Flask(__name__)
 def hello():
     return "Hello World!"
 
+@app.route('/secret')
+def hidden():
+    return "Welcome to the hidden place"
+
 if __name__ == '__main__':
     port = os.environ.get('FLASK_PORT') or 8080
     port = int(port)
